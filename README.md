f0b1ed91d5216bb0d42bad214bc60df94b8a6701
supperchong <supperchong@gmail.com>
2019-04-04 14:31:57 +0800

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# prettyJson README
+
+The prettier or vscode-json beautify can format pretty json,but they don't format javascript object,such as 
+```js
+{name:1}
+
+```
+The prettyJson is more power than vscode-json beautify.PrettyJson can format pretty any javascript object.
+
+```js
+{name:"li"}
+=>
+{
+    "name":"li"
+}
+
+{'name':'li'}
+=>
+{
+    "name":"li"
+}
+
+[{name:'li',age:2}]
+=>
+[
+    {
+        "name": "li",
+        "age": 2
+    }
+]
+```
+
+The prettyJson is more power than prettier when format pretty json,it support pretty partial code in any file.
+
---
+++ src/extension.ts
@@ -0,0 +1,41 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import {TextEditor,TextEditorEdit} from 'vscode';
+const pretty=require('pretty-object-string');
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "json" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerTextEditorCommand('extension.prettyJson', (textEditor: TextEditor, edit: TextEditorEdit, args: any[]) => {
+        // The code you place here will be executed every time your command is executed
+        let selectText=textEditor.document.getText(textEditor.selections[0]);
+        let prettyText:string;
+        try{
+            prettyText=pretty(selectText,{delComment:true});
+        }catch(err){
+            vscode.window.showInformationMessage(err.message);
+            return;
+        }
+        
+        textEditor.edit((editBuilder:TextEditorEdit)=>{
+            editBuilder.replace(textEditor.selections[0],prettyText);
+        });
+        // Display a message box to the user
+        // vscode.window.showInformationMessage('Hello World!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
